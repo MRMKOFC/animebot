00d018464d47c1a0de69a1a206067d5b0d663350
MK <115228027+MRMKOFC@users.noreply.github.com>
2025-03-22 18:30:04 +0530

    Update animebot.py

--- animebot.py
+++ animebot.py
@@ -149,7 +149,7 @@ def send_to_telegram(title, image_url, summary):
     safe_title = escape_markdown(title)
     safe_summary = escape_markdown(summary) if summary else "No summary available"
     
-    caption = f"⚡ *{safe_title}* ⚡\n\n {safe_summary}\n\n🍁|`@TheAnimeTimes_acn` "
+    caption = f"⚡ *{safe_title}* ⚡\n\n {safe_summary}\n\n 🍁:`@TheAnimeTimes_acn` "
     params = {"chat_id": CHAT_ID, "caption": caption, "parse_mode": "MarkdownV2"}
 
     try:
